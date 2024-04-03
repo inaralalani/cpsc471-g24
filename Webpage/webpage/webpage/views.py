429f5496170e684cd180c8715fdbe65a15c82e9e
Larryman555 <larryman555@gmail.com>
2024-04-03 13:53:46 -0600

    Django setup
    works?

---
+++ Webpage/webpage/webpage/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+def landing(request):
+    template = loader.get_template('landingpage.html')
+    return HttpResponse(template.render())
+def options(request):
+    template = loader.get_template('options.html')
+    return HttpResponse(template.render())
+def rlogin(request):
+    template = loader.get_template('researchers-login.html')
+    return HttpResponse(template.render())
+def llogin(request):
+    template = loader.get_template('labelers-login.html')
+    return HttpResponse(template.render())
+def dashboard(request):
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render())
