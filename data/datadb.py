33e7a81f8c7ce608093a03d3afe3f186a9a1edfb
inaralalani <79676507+inaralalani@users.noreply.github.com>
2024-04-04 16:01:59 -0600

    update DMBS

--- data/datadb.py
+++ data/datadb.py
@@ -1,11 +1,11 @@
 import duckdb as ddb
+
 con = ddb.connect("m2kdashboard.db")
 
-ddb.read_csv("LABELS.csv")
-ddb.read_csv("FOODS.csv")
-ddb.read_csv("MARKETING.csv")
-ddb.read_csv("TECHNIQUES.csv")
-ddb.read_csv("USERS.csv")
-
-
+con.read_csv("data/*.csv")
+con.sql("CREATE OR REPLACE TABLE Labels AS SELECT * FROM 'data/LABELS.csv';")
+con.sql("CREATE OR REPLACE TABLE Foods AS SELECT * FROM 'data/FOODS.csv';")
+con.sql("CREATE OR REPLACE TABLE Marketing AS SELECT * FROM 'data/MARKETING.csv';")
+con.sql("CREATE OR REPLACE TABLE Techniques AS SELECT * FROM 'data/TECHNIQUES.csv';")
+con.sql("CREATE OR REPLACE TABLE Users AS SELECT * FROM 'data/USERS.csv';")
 
