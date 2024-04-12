fd36a515d7c0f37e7b7a58058598d02e5db5be61
inaralalani <79676507+inaralalani@users.noreply.github.com>
2024-04-12 01:31:19 -0600

    change db creation per updated data

--- data/datadb.py
+++ data/datadb.py
@@ -2,9 +2,9 @@ import duckdb as ddb
 
 con = ddb.connect("m2kdashboard.db")
 con.read_csv("data/*.csv")
-con.sql("CREATE OR REPLACE TABLE Labels AS SELECT * FROM 'data/USERS.csv';")
-con.sql("CREATE OR REPLACE TABLE Foods AS SELECT * FROM 'data/INSTANCES.csv';")
+con.sql("CREATE OR REPLACE TABLE Users AS SELECT * FROM 'data/USERS.csv';")
+con.sql("CREATE OR REPLACE TABLE MarketingInstances AS SELECT * FROM 'data/INSTANCES.csv';")
 con.sql("CREATE OR REPLACE TABLE Marketing AS SELECT * FROM 'data/MARKETING.csv';")
 con.sql("CREATE OR REPLACE TABLE Techniques AS SELECT * FROM 'data/TECHNIQUES.csv';")
-con.sql("CREATE OR REPLACE TABLE Users AS SELECT * FROM 'data/FOODS.csv';")
-con.sql("CREATE OR REPLACE TABLE Users AS SELECT * FROM 'data/HEALTHY.csv';")
+con.sql("CREATE OR REPLACE TABLE Foods AS SELECT * FROM 'data/FOODS.csv';")
+con.sql("CREATE OR REPLACE TABLE Healthy AS SELECT * FROM 'data/HEALTHY.csv';")
